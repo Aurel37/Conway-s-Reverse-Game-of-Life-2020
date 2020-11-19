e41ba2cffbcc6205ba214166dfcd48ec48a69295
Sébastien Morel <sebastien.morel@eleves.enpc.fr>
2020-11-19 18:33:03 +0100

    Mise en place de l'affichage

--- gameClass.py
+++ gameClass.py
@@ -1,5 +1,8 @@
 import numpy as np
 import random as rd
+from time import sleep
+import matplotlib.pyplot as plt
+
 
 
 class grid:
@@ -7,6 +10,11 @@ class grid:
         self.n = n
         self.p = p
         self.mat = np.zeros((n, p))
+        self.col = ['red', 'green']
+
+    def initialization(self, matrix):
+        if matrix.shape() == [self.n, self.p]:
+            self.mat = np.copy(matrix)
 
     def setCell(self, i, j):
         """Useful to init the grid"""
@@ -19,7 +27,7 @@ class grid:
                 if (rd.random() > 0.6):
                     self.mat[i, j] = 1
 
-    def setState(self, i, j):
+    def setState(self, i, j, mat):
         """Considering a position in the grid
            Update the position"""
         cases = [(1, 1), (-1, -1), (1, -1), (-1, 1), (1, 0), (0, 1), (-1, 0), (0, -1)]
@@ -27,17 +35,48 @@ class grid:
         if (0 <= i < self.n and 0 <= j < self.p):
             for case in cases:
                 try:
-                    livingCell += self.mat[i + case[0], j + case[1]]
+                    livingCell += mat[i + case[0], j + case[1]]
                 except:
                     pass 
             if (livingCell > 3 or livingCell < 2):
                 self.mat[i, j] = 0
-            elif (self.mat[i, j] == 0 and livingCell == 3):
+            elif (mat[i, j] == 0 and livingCell == 3):
                 self.mat[i, j] = 1
+    
 
-    def runGame(self, Time):
-        for t in range(Time):
-            for i in range(self.n):
-                for j in range(self.p):
-                    self.setState(i, j)
-            print(self.mat, end='\r')
+
+    def oneStep(self):
+        mat = self.mat.copy()
+        for i in range(self.n):
+            for j in range(self.p):
+                self.setState(i, j, mat)
+    
+    def printGrid(self, ax):
+        for i in range(self.n):
+            for j in  range(self.p):
+                a_circle = plt.Circle((i + 0.5, j + 0.5), .45, color = self.col[int(self.mat[i, j])])
+                ax.add_artist(a_circle)
+
+
+    def showGame(self, time):
+
+        fig, ax = plt.subplots()
+        ax.set(xlim=(0, self.n), ylim = (0, self.p))
+        self.printGrid(ax)
+        for _ in range(time):
+            plt.pause(0.5)
+            self.oneStep()
+            self.printGrid(ax)
+
+
+    def game(self, time):
+        for _ in range(time):
+            self.oneStep()
+            print(self.mat)
+
+
+
+test = grid(5, 5)
+
+test.setRandom()
+test.showGame(10)
